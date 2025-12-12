74a5f38380bd90540e8ae3adff351de629c4b568
Axo Hossou <axo.hossou@epitech.eu>
2025-12-12 17:24:33 +0100

    feat: add modification in all files

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./File-html/**/*.html"],
+  content: [
+    "./File-html/**/*.html",
+    "./File-js/**/*.js",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}   
+}
